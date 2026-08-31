058c26194aac19a375b116f320f7629a67a2d2ec
afsyanr <afsyanurr@gmail.com>
2026-08-31 13:04:11 +0700

    add tentang

---
+++ jelajahpo-frontend/src/pages/Tentang.jsx
@@ -0,0 +1,15 @@
+export default function Tentang() {
+    return (
+        <div className="row text-start">
+            <div className="col-10">
+                <h2 className="fw-bold mb-3">Tentang Saya</h2>
+
+                <p>Hallo nama saya RAMA</p>
+
+                <p>XII RPL 1</p>
+
+                <p>Saya ingin berkunjung di.....</p>
+            </div>
+        </div>
+    )
+}
